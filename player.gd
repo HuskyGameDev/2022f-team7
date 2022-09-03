dc3351855a2bfe43c15cec1d16fa771d9e09ca8f
Soup <bmanbros@gmail.com>
2022-09-02 23:03:24 -0400

    added a test level and player boilerplate code
    added a debug level and a player that can be controlled, took a while to fix some issues with handling slopes and issues with physics jitter that seemed to be caused by bad camera smoothing, so that's disabled for now.

--- player.gd
+++ player.gd
@@ -10,7 +10,35 @@ extends KinematicBody2D
 func _ready():
 	pass # Replace with function body.
 
+var vec = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	
+	if !is_on_floor():
+		vec.y += delta * 400 
+		#gravity, only this accounts for frame time differences since move/slide already accounts for this somewhat, but gravity is additive
+	else:
+		vec.y = 1
+	
+	
+	vec.x = 0
+	
+	if Input.is_action_just_pressed("dash"):
+		pass #no dash implemented
+	if Input.is_action_pressed("moveDown"):
+		pass #for slam or crouch if needed
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		vec.y = -400
+	if Input.is_action_pressed("moveLeft"):
+		vec.x += -175
+	if Input.is_action_pressed("moveRight"):
+		vec.x += 175
+	
+	print(is_on_floor())
+	
+	move_and_slide(vec, Vector2.UP, true, 4, deg2rad(45), true)
+
+
+func _process(delta):
+	pass
+
