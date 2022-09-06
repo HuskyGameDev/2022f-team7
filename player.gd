acaf514144fe43dc80991972d62455d11e73853c
Soup <bmanbros@gmail.com>
2022-09-05 20:40:16 -0400

    dash and other tweaks
    added dash to player, finished besides minor tweaking
    adjusted player camera
    added old player sprite which flips based on movement direction
    cleaned up some of the player code to have a better spot for tweaking the movement behavior
    enabled msaa in project settings, as well as enabling multi-threading

--- player.gd
+++ player.gd
@@ -1,44 +1,65 @@
 extends KinematicBody2D
 
+export var gravity = 400
+export var jumpPower = 325
+export var moveSpeed = 175
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var dashDistance = 150 
+#this needs to be adapted to dash time because it ends based on time so the dash ends consistently but interpolating to a "desired" point still results in the best feeling movement
 
+var isDashing = false
+var canDash = true
+var dashStart = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass #leaving this in event of player needing init code
 
 var vec = Vector2.ZERO
 
 func _physics_process(delta):
 	
+	$hud/debugInf.text = String(self.position)
+	
 	if !is_on_floor():
-		vec.y += delta * 400 
-		#gravity, only this accounts for frame time differences since move/slide already accounts for this somewhat, but gravity is additive
+		vec.y += delta * gravity 
 	else:
 		vec.y = 1
-	
 	
 	vec.x = 0
 	
 	if Input.is_action_just_pressed("dash"):
-		pass #no dash implemented
+		if !isDashing && canDash:
+			isDashing = true
+			$timer.start()
+			dashStart = self.position.x
 	if Input.is_action_pressed("moveDown"):
 		pass #for slam or crouch if needed
 	if Input.is_action_just_pressed("jump") && is_on_floor():
-		vec.y = -400
-	if Input.is_action_pressed("moveLeft"):
-		vec.x += -175
-	if Input.is_action_pressed("moveRight"):
-		vec.x += 175
+		vec.y = -jumpPower
+	if Input.is_action_pressed("moveLeft") && !isDashing:
+		vec.x += -moveSpeed
+	if Input.is_action_pressed("moveRight") && !isDashing:
+		vec.x += moveSpeed
 	
-	print(is_on_floor())
+	if vec.x < 0: #code to flip the sprite based on movement direction
+		$Sprite.flip_h = true
+	elif vec.x > 0:
+		$Sprite.flip_h = false
+	
+	if isDashing:
+		
+		if $Sprite.flip_h:
+			self.position.x = lerp(self.position.x, (dashStart - dashDistance), 0.15)
+		else:
+			self.position.x = lerp(self.position.x, (dashStart + dashDistance), 0.15)
 	
 	move_and_slide(vec, Vector2.UP, true, 4, deg2rad(45), true)
 
+func _on_Timer_timeout():
+	isDashing = false
+	canDash = false
+	$timer.stop()
+	$dashCooldown.start()
 
-func _process(delta):
-	pass
-
+func _on_dashCooldown_timeout():
+	canDash = true
