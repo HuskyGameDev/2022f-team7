3c6b06ec5bc1459eb2e97164079da1ac67f8b594
Soup <bhcobb@mtu.edu>
2022-09-10 17:51:02 -0400

    new dash and some other misc stuff
    +new dash that works a lot better with less quirks and less code
    +moved spear spawning to _input() to save on performance
    +player internally keeps track of kinematic velocity for the new dash system and debugging
    +more files added to gitignore

--- player.gd
+++ player.gd
@@ -5,27 +5,23 @@ export(PackedScene) var spearScene
 export var gravity = 400
 export var jumpPower = 350
 export var moveSpeed = 175
+export var dashSpeed = 5000
 
-export var dashDistance = 150 
-#this needs to be adapted to dash time because it ends based on time so the dash ends consistently but interpolating to a "desired" point still results in the best feeling movement
 
 var isDashing = false
 var canDash = true
-var dashStart = 0
-
-func _ready():
-	pass #leaving this in event of player needing init code
-
 var vec = Vector2.ZERO
+var vel = Vector2.ZERO
 
-func _process(delta):
+func _input(event):
 	if Input.is_action_just_pressed("mouseLeft"):
 		call_deferred("throwSpear")
 
 func _physics_process(delta):
-	print(vec.y)
+	var space_state = get_world_2d().direct_space_state
+	var result
 	
-	$hud/debugInf.text = String(self.position)
+	$hud/debugInf.text = String(self.position) + "\n" + String(vel)
 	
 	if !is_on_floor():
 		vec.y += delta * gravity 
@@ -42,7 +38,6 @@ func _physics_process(delta):
 		if !isDashing && canDash:
 			isDashing = true
 			$timer.start()
-			dashStart = self.position.x
 	if Input.is_action_pressed("moveDown"):
 		pass #for slam or crouch if needed
 	if Input.is_action_just_pressed("jump") && is_on_floor():
@@ -58,22 +53,15 @@ func _physics_process(delta):
 		$Sprite.flip_h = false
 	
 	if isDashing:
-		
-		var space_state = get_world_2d().direct_space_state
-		var result
-		
+		if abs(vel.x) < 95 && (.2 - $timer.time_left) > .05:
+			dashEnd()
+			print("end")
 		if $Sprite.flip_h:
-			if(space_state.intersect_ray(position, Vector2(position.x - 11, position.y + 28), [self]).size() > 0):
-				dashEnd()
-			else:
-				self.position.x = lerp(self.position.x, (dashStart - dashDistance), 0.1)
+			vec.x = -dashSpeed*pow(.000000001, $timer.wait_time - $timer.time_left)
 		else:
-			if(space_state.intersect_ray(position, Vector2(position.x - 11, position.y - 28), [self]).size() > 0):
-				dashEnd()
-			else:
-				self.position.x = lerp(self.position.x, (dashStart + dashDistance), 0.1)
-			
-	move_and_slide_with_snap(vec, Vector2.DOWN, Vector2.UP, true, 4, deg2rad(45), true)
+			vec.x = dashSpeed*pow(.000000001, $timer.wait_time - $timer.time_left)
+	
+	vel = move_and_slide_with_snap(vec, Vector2.DOWN, Vector2.UP, true, 4, deg2rad(45), true)
 
 func _on_Timer_timeout():
 	dashEnd()
