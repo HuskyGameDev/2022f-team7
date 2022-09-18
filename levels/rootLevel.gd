e2441e04d6e8add77a8987c4924c3db0a22db64e
Soup <bhcobb@mtu.edu>
2022-09-17 23:00:15 -0400

    some minor refactoring
    made a root scene that should be instantiated and the parent of everything in a level so it can act as a base state management script for levels (including the pause menu)

---
+++ levels/rootLevel.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+
+#root level scene for game state management controller, empty because I'm not
+#assigned to this stuff and don't want to overreach again :) -soup
+
+func _ready():
+	pass # Replace with function body.
--- main.gd
+++ main.gd
@@ -14,21 +14,13 @@ func _on_play_pressed():
 func _on_back_pressed():
 	$levelSelect.visible = false
 	$playScreen.visible = true
-	
-func _on_pause_pressed():
-	print("paused")
-	get_tree().pause = true
-	$pauseScreen.visible = true
-		
 
 func _on_return_pressed():
 	get_tree().pause = false
 	$pauseScreen.visible = false
 
-
 func _on_quit_pressed():
 	get_tree().quit()
-
 
 func _on_Debug_pressed():
 	get_tree().change_scene("res://levels/debug level.tscn")
