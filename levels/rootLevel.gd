761d39cfb8fac2063d536b985a25ae3f3c259eda
Owen Crosby <90730357+UdderNonsense@users.noreply.github.com>
2022-09-21 22:06:17 -0400

    Pause Menu Rework
    Removed GUI scene, set pauseScreen to canvaslayer instead of Control

--- levels/rootLevel.gd
+++ levels/rootLevel.gd
@@ -7,8 +7,31 @@ extends Node2D
 signal game_paused
 
 func _ready():
-	$GUI/pauseScreen.visible = false
+	$pauseScreen.visible = false
+	
+func _on_resume_pressed():
+	print("returnpressed")
+	get_tree().paused = false
+	$pauseScreen.visible = false
+
+func _on_quit_pressed():
+	print("quitpressed")
+	get_tree().quit()
+
+func _on_levelRoot_game_paused():
+	$pauseScreen.visible = !$pauseScreen.visible
+	get_tree().paused = !get_tree().paused
+
+
+func _on_menu_pressed():
+	get_tree().change_scene("res://main.tscn")
+	get_tree().paused = false
+
 	
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("pause_game"):
 		emit_signal("game_paused")
+
+
+func _on_game_paused():
+	pass # Replace with function body.
