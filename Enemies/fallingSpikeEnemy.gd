3b6800eeebf93d88cbc22b892672d568b1bdab0d
iwsandfo <iwsandfo@mtu.edu>
2022-10-12 21:44:24 -0400

    Level 1-1 Updates
    Added some flamey head enemies to the first level and gave them all unique paths. Also tried updating the falling spike, but at the moment, it just disappears when it registers the player beneath it.

--- Enemies/fallingSpikeEnemy.gd
+++ Enemies/fallingSpikeEnemy.gd
@@ -19,40 +19,43 @@ func _ready():
 
 
 func _physics_process(delta):
-	if(aiming): rotateSpike()
+	#if(aiming): rotateSpike()
 	if(attacking): hit = move_and_collide(attackDirection * speed * delta)
 	if(hit != null && !hit.collider.is_in_group("tilemap")): queue_free()
 	
 func _on_StartRange_body_entered(body):
-	if(!aimed && body.is_in_group("player")):
-		$AimTimer.start()
-		aiming = true
+	#if(!aimed && body.is_in_group("player")):
+	if(body.is_in_group("player")):
+	#	$AimTimer.start()
+	#	aiming = true
+		$AttackDelay.start()
 
 
-func _on_AimTimer_timeout():
-	aiming = false
-	aimed = true
-	$AttackDelay.start()
+#func _on_AimTimer_timeout():
+#	aiming = false
+#	aimed = true
+#	$AttackDelay.start()
 	
 	
-func rotateSpike():
-	direction = Vector2($"../player".get_global_position().x, $"../player".get_global_position().y) # Get player location
-	angle = get_global_position().angle_to_point(direction) + (3.0/2.0)*PI # Get angle to point toward player
+#func rotateSpike():
+#	direction = Vector2($"../player".get_global_position().x, $"../player".get_global_position().y) # Get player location
+#	angle = get_global_position().angle_to_point(direction) + (3.0/2.0)*PI # Get angle to point toward player
 	# Adjust rotation/angle so that it doesn't rotate the long way around
-	if abs(rotation - angle) >= PI && rotation != 0:
-		if rotation > angle:
-			rotation = (fposmod(rotation + PI, 2.0*PI) - PI) - (2.0 * PI)
-		else:
-			angle = (fposmod(angle + PI, 2.0*PI) - PI) - (2.0 * PI)
+#	if abs(rotation - angle) >= PI && rotation != 0:
+#		if rotation > angle:
+#			rotation = (fposmod(rotation + PI, 2.0*PI) - PI) - (2.0 * PI)
+#		else:
+#			angle = (fposmod(angle + PI, 2.0*PI) - PI) - (2.0 * PI)
 	# Set rotation
-	if(rotation > angle + aimSpeed): rotation -= aimSpeed
-	if(rotation < angle - aimSpeed): rotation += aimSpeed
+#	if(rotation > angle + aimSpeed): rotation -= aimSpeed
+#	if(rotation < angle - aimSpeed): rotation += aimSpeed
 
 
 func _on_AttackDelay_timeout():
 	attacking = true
-	attackDirection = get_global_position().direction_to(direction).normalized()
-
+	direction = Vector2(0, -1)
+	#attackDirection = get_global_position().direction_to(direction).normalized()
+	attackDirection = get_global_position() - direction
 
 func _on_Tip_body_entered(body):
 	if(body.is_in_group("tilemap") && attacking): queue_free()
