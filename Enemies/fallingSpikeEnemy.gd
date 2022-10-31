c95eaddf771a272d3e630fb0983ddcac8d911fe8
iwsandfo <iwsandfo@mtu.edu>
2022-10-31 19:00:56 -0400

    Updated level 1-1 enemy layout
    With some of the other recent updates, the enemies in level 1-1 weren't showing up, so I added another set of enemies, and gave them new paths. The paths are simpler, so hopefully they will be easier for the player to predict.

--- Enemies/fallingSpikeEnemy.gd
+++ Enemies/fallingSpikeEnemy.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
+export (PackedScene) var particles;
+
 var aiming = false
 var aimed = false
 var attacking = false
@@ -19,43 +21,46 @@ func _ready():
 
 
 func _physics_process(delta):
-	#if(aiming): rotateSpike()
-	if(attacking): hit = move_and_collide(attackDirection * speed * delta)
-	if(hit != null && !hit.collider.is_in_group("tilemap")): queue_free()
+	if(aiming): rotateSpike()
+	if(attacking): hit = move_and_collide(attackDirection * speed * delta, true)
+	#if(hit != null && !hit.collider.is_in_group("tilemap")): queue_free()
 	
 func _on_StartRange_body_entered(body):
-	#if(!aimed && body.is_in_group("player")):
-	if(body.is_in_group("player")):
-	#	$AimTimer.start()
-	#	aiming = true
-		$AttackDelay.start()
+	if(!aimed && body.is_in_group("player")):
+		$AimTimer.start()
+		aiming = true
 
 
-#func _on_AimTimer_timeout():
-#	aiming = false
-#	aimed = true
-#	$AttackDelay.start()
+func _on_AimTimer_timeout():
+	aiming = false
+	aimed = true
+	$AttackDelay.start()
 	
 	
-#func rotateSpike():
-#	direction = Vector2($"../player".get_global_position().x, $"../player".get_global_position().y) # Get player location
-#	angle = get_global_position().angle_to_point(direction) + (3.0/2.0)*PI # Get angle to point toward player
+func rotateSpike():
+	direction = Vector2($"../player".get_global_position().x, $"../player".get_global_position().y) # Get player location
+	angle = get_global_position().angle_to_point(direction) + (1.0/2.0)*PI # Get angle to point toward player
 	# Adjust rotation/angle so that it doesn't rotate the long way around
-#	if abs(rotation - angle) >= PI && rotation != 0:
-#		if rotation > angle:
-#			rotation = (fposmod(rotation + PI, 2.0*PI) - PI) - (2.0 * PI)
-#		else:
-#			angle = (fposmod(angle + PI, 2.0*PI) - PI) - (2.0 * PI)
+	if abs(rotation - angle) >= PI && rotation != 0:
+		if rotation > angle:
+			rotation = (fposmod(rotation + PI, 2.0*PI) - PI) - (2.0 * PI)
+		else:
+			angle = (fposmod(angle + PI, 2.0*PI) - PI) - (2.0 * PI)
 	# Set rotation
-#	if(rotation > angle + aimSpeed): rotation -= aimSpeed
-#	if(rotation < angle - aimSpeed): rotation += aimSpeed
+	if(rotation > angle + aimSpeed): rotation -= aimSpeed
+	if(rotation < angle - aimSpeed): rotation += aimSpeed
 
 
 func _on_AttackDelay_timeout():
 	attacking = true
-	direction = Vector2(0, -1)
-	#attackDirection = get_global_position().direction_to(direction).normalized()
-	attackDirection = get_global_position() - direction
+	attackDirection = get_global_position().direction_to(direction).normalized()
+	$Tip.set_collision_mask_bit(1, true)
+
 
 func _on_Tip_body_entered(body):
-	if(body.is_in_group("tilemap") && attacking): queue_free()
+	if(attacking):
+		var p = particles.instance()
+		p.transform = transform
+		get_parent().add_child(p)
+		p.emitting = true;
+		queue_free()
