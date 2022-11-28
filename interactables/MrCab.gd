186d62d962cdfa29718cb3aa6fe85722e6d22de4
Cecilia Schmitz <ceciliamschmitz@gmail.com>
2022-11-28 13:29:58 -0500

    Spear can now be attached to Mr. Cab
    Still a bit buggy. Also had to use collision layer 5 for spear collision reasons.

--- interactables/MrCab.gd
+++ interactables/MrCab.gd
@@ -4,7 +4,11 @@ extends KinematicBody2D
 export var direction = false # False for left, true for right.
 export var speed = 15
 export var gravity = 50
-var cooldown = false
+export var ray_near_cast = 5
+export var ray_far_cast = 17
+var cooldown = false # Cooldown so that Mr.Cab doesn't spam direction changes
+var spear_attached = false
+var spear_direction = false # false if the spear is attached to the left side, true if attached to the right side
 
 
 func _ready():
@@ -14,24 +18,46 @@ func _ready():
 	$RayCast2D.cast_to = Vector2(abs($RayCast2D.cast_to.x), 0) if direction else Vector2(-abs($RayCast2D.cast_to.x), 0)
 
 
-func _process(delta):
+func _physics_process(delta):
+	if($RayCast2D.get_collider() != null && $RayCast2D.get_collider().name != "Spear" && !cooldown) : 
+		flipDirection()
 	var yvel = gravity if !is_on_floor() else 0
 	move_and_slide(Vector2(speed, yvel), Vector2.UP)
 
 
-func _physics_process(delta):
-	if($RayCast2D.get_collider() != null && !cooldown) : flipDirection()
-
-
 func flipDirection():
-	print("flipping direction")
 	direction = !direction
 	$AnimatedSprite.flip_h = direction;
 	speed = abs(speed) if direction else -abs(speed);
-	$RayCast2D.cast_to = Vector2(abs($RayCast2D.cast_to.x), 0) if direction else Vector2(-abs($RayCast2D.cast_to.x), 0)
+	update_ray()
 	$Cooldown.start()
 	cooldown = true;
 
 
 func _on_Cooldown_timeout():
-	cooldown =false
+	cooldown = false
+
+# Updates the direction and length of the raycast, depending on the direction and spear_attached variables
+func update_ray():
+	if(spear_attached):
+		if(direction):
+			$RayCast2D.cast_to = Vector2(ray_far_cast, 0) if spear_direction else Vector2(ray_near_cast, 0)
+		else:
+			$RayCast2D.cast_to = Vector2(-ray_far_cast, 0) if !spear_direction else Vector2(-ray_near_cast, 0)
+	else:
+		$RayCast2D.cast_to = Vector2(ray_near_cast, 0) if direction else Vector2(-ray_near_cast, 0)
+
+
+# Called when the spear collides with Mr. Cab
+func _on_SpearDetection_body_entered(body):
+	if(!spear_attached && body.name == "Spear"):
+		body.connect("spear_collected", self, "remove_spear")
+		spear_direction = false if body.global_position.x < global_position.x else true
+		spear_attached = true
+		update_ray()
+
+
+# Called when the player collects the spear
+func remove_spear():
+	spear_attached = false
+	update_ray()
