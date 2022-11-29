2f621acd75bae6f40a71b2d0f81d19fb3837f945
Andrew Tran <altran@mtu.edu>
2022-11-28 20:25:04 -0500

    pegasus enemy stuff
    finished up the rest of the pegasus enemy coding, just need to do some testing and have the sprites for the enemy

--- Enemies/pegasusEnemy.gd
+++ Enemies/pegasusEnemy.gd
@@ -10,14 +10,35 @@ var aimSpeed = 0.02
 var attacking = false
 var attackDirection
 var hit
+var location
+var returning
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+func _physics_process(delta):
+	match(mode):
+		0:
+			var playerPos = $"../player".get_global_position() - self.global_position
+			if engaged:
+				if playerPos.length < 1:
+					pass
+				else:
+					vec = (position - player.position).normalized()
+					vec *= -0.2
+					move_and_collide(vec, false)
+		1:#MUST be a child of a pathfollower!
+			self.get_parent().unit_offset += (.0005 * railSpeed)
+		2:
+			pass #have not finished yet, no enemies use it either
+		3:
+			customMode(delta)
+
 func _onStartEnter(body):
 	._onStartEnter(body)
 	if(!aimed && body.is_in_group("player") && charge):
 		charge = false
+		location = self.get_global_position()
 		$AimTimer.start()
 		aiming = true
 		
@@ -26,6 +47,7 @@ func _onStartEnter(body):
 func custom(delta):
 	if(aiming): rotatePegasus()
 	if(attacking): hit = move_and_collide(attackDirection * speed)
+	if(returning): position = position.move_toward(location, speed*delta)
 	.customMode(delta)
 	
 	
@@ -46,10 +68,10 @@ func _on_hitbox_area_entered(area):
 	._on_hitbox_area_entered(area)
 	if(hit != null && hit.collider.is_in_group("tilemap")):
 		returnPosition()
-		$chargeCooldown.start()
 	
 func returnPosition():
-	pass
+	returning = true
+	$chargeCooldown.start()
 	
 func _on_chargeCooldown_timeout():
 	charge = true
@@ -63,3 +85,4 @@ func _on_chargeUp_timeout():
 	attacking = true
 	attackDirection = get_global_position().direction_to(direction).normalized()
 	$animatedSprite.frame = engaged
+	
