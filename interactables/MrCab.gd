1a23432511d41d49edbef4983bff1a166932a7ba
Soup <bhcobb@mtu.edu>
2022-11-29 12:34:09 -0500

    fix cab getting stuck on slopes
    +mr cab now uses slope snapping to walk up slopes
    +mr cab also follows move and slide
    use conventions

--- interactables/MrCab.gd
+++ interactables/MrCab.gd
@@ -10,28 +10,32 @@ export var npc = false #if mr cab has anything to say
 var cooldown = false # Cooldown so that Mr.Cab doesn't spam direction changes
 var spear_attached = false
 var spear_direction = false # false if the spear is attached to the left side, true if attached to the right side
-var yvel = 0
+#var yvel = 0
+
+var vec = Vector2.ZERO
 
 
 func _ready():
 	$AnimatedSprite.flip_h = direction;
 	$AnimatedSprite.play("walk");
 	speed = abs(speed) if direction else -abs(speed);
+	vec.x = speed
 	$RayCast2D.cast_to = Vector2(abs($RayCast2D.cast_to.x), 0) if direction else Vector2(-abs($RayCast2D.cast_to.x), 0)
 
 
 func _physics_process(delta):
 	if($RayCast2D.get_collider() != null && $RayCast2D.get_collider().name != "Spear" && !cooldown) : 
 		flipDirection()
-	yvel += gravity if !is_on_floor() else 0
-	yvel = clamp(yvel, -10000, 600)
-	move_and_slide(Vector2(speed, yvel), Vector2.UP)
+	vec.y += gravity if !is_on_floor() else 0
+	vec.y = clamp(vec.y, -10000, 600)
+	vec = move_and_slide_with_snap(vec, Vector2.DOWN ,Vector2.UP, true, 4, deg2rad(45), true)
 
 
 func flipDirection():
 	direction = !direction
 	$AnimatedSprite.flip_h = direction;
 	speed = abs(speed) if direction else -abs(speed);
+	vec.x = speed
 	update_ray()
 	$Cooldown.start()
 	cooldown = true;
