861239c124f5f4675143199fba05367a406e1a75
Andrew Tran <altran@mtu.edu>
2023-02-01 19:17:15 -0500

    added a "key" for the moving platforms
    right now, it's not interactable but just a key that the player that can be obtained and immediately causes the platforms to move. might change into something like a pressure plate later or a switch.

--- Levels/platform.gd
+++ Levels/platform.gd
@@ -4,14 +4,17 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("platform")
+	#$AnimationPlayer.play("platform")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+
+
+
+func _on_KeyPlatform_ActivatedMovingPlatforms():
+	$AnimationPlayer.play("platform")
+	pass
