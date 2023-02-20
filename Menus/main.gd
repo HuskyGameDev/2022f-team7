7496cd2d9ca75b66fcf4be7325e21b926f6a4869
iwsandfo <iwsandfo@mtu.edu>
2023-02-19 23:09:45 -0500

    Attached new levels to main menu
    Added two new buttons to the main menu for the two new levels.

--- Menus/main.gd
+++ Menus/main.gd
@@ -36,6 +36,12 @@ func _on_levelThree_pressed():
 
 func _on_levelFour_pressed():
 	get_tree().get_root().get_child(0).load_level("res://Levels/level1-4.tscn")
+	
+func _on_levelFive_pressed():
+	get_tree().get_root().get_child(0).load_level("res://Levels/level1-5.tscn")
+	
+func _on_levelSix_pressed():
+	get_tree().get_root().get_child(0).load_level("res://Levels/level2-1.tscn")
 
 func _on_tutorialLevel_pressed():
 	get_tree().get_root().get_child(0).load_level("res://Levels/tutorialLevel.tscn")
