cef9284ad124c53d4b0151ea15d2c2f9f6641695
Soup <bhcobb@mtu.edu>
2023-03-14 20:22:01 -0400

    clean up mr jab fixes
    +refactored the changes to mr jab to be more efficient and easier to read

--- Enemies/mrJab.gd
+++ Enemies/mrJab.gd
@@ -17,41 +17,38 @@ var playerBody
 
 var returnVec = Vector2.ZERO
 
-#boilerplate methods
-func _ready():
-	pass
-	#rotation = -2*PI
-
 #enemy mode override (called under physics process)
 func customMode(delta):
-	if(playerBody != null && playerBody.is_in_group("player")):
-		result = get_world_2d().direct_space_state.intersect_ray($losCast.global_position, playerBody.global_position, [self, playerBody], collision_mask)
-		if result.size() == 0:
-			_onStartEnter(playerBody)
+	
+	if(engaged && playerBody != null && lineOfSight(playerBody)):
+		_onStartEnter(playerBody)
+	
 	if(aiming): rotateSpike()
+	
 	if(attacking): 
 		returnVec = move_and_slide(attackDirection * speed)
 		if(returnVec.length() < 50): #if going too slow
 			_onTipHit(null)
+	
 	.customMode(delta)
 
-func _onStartEnter(body):
-	._onStartEnter(body)
-	playerBody = body
-	result = get_world_2d().direct_space_state.intersect_ray($losCast.global_position, playerBody.global_position, [self, playerBody], collision_mask)
-	print(body.is_in_group("player"))
-	print(result.size())
-	print(!aimed)
-	if aiming || aimed: return
-	if(body.is_in_group("player") && result.size() == 0 && $VisibilityNotifier2D.is_on_screen()):
-		$VisibilityNotifier2D.queue_free()
-		$AimTimer.start()
-		aiming = true
-		$AnimatedSprite.animation = "alert"
-		$AnimatedSprite.playing = true
-		set_collision_layer_bit(4, true)
-
 #signals
+
+func _onStartEnter(body):
+	._onStartEnter(body)#super call
+	
+	playerBody = body
+	
+	if( !(aiming || aimed) && lineOfSight(body) ):
+		print("locking on!")
+		alertSpike()
+	else:
+		print("no lock, waiting for lock")
+
+func _onStopExit(body):
+	._onStopExit(body)#super call
+	print("stopping lockon")
+
 func _onAimEnd():
 	aiming = false
 	aimed = true
@@ -76,6 +73,24 @@ func _on_hitbox_area_entered(area):
 		._on_hitbox_area_entered(area)
 
 #helper methods
+
+func lineOfSight(var body:Node2D):
+	result = get_world_2d().direct_space_state.intersect_ray($losCast.global_position, playerBody.global_position, [self, playerBody], collision_mask)
+	return result.size() == 0 && $VisibilityNotifier2D.is_on_screen()
+
+func alertSpike():
+	engaged = false
+	$startRange.monitoring = false
+	$stopRange.monitoring = false
+	$VisibilityNotifier2D.queue_free() #no need for this
+	
+	aiming = true
+	$AimTimer.start()
+	
+	$AnimatedSprite.animation = "alert"
+	$AnimatedSprite.playing = true
+	set_collision_layer_bit(4, true)
+
 func rotateSpike():
 	direction = Vector2($"../player".get_global_position().x, $"../player".get_global_position().y) # Get player location
 	angle = get_global_position().angle_to_point(direction) + (3.0/2.0)*PI # Get angle to point toward player
