41df9426bad7b70cabaaa057f4f8da1fcc4524f2
iwsandfo <iwsandfo@mtu.edu>
2023-03-15 12:13:50 -0400

    Minor changes for playtesting
    -Moved the spawn point in 2-1 to the correct position
    
    -Disabled 2-2 in the main menu
    
    -Changed the elevator in 2-1 so that it doesn't take the player to 2-2
    
    -Commented out some of the platform.gd script since it was preventing the platforms from playing their animations. It's still in the script since I'm assuming someone is going to be using it for something, but at the moment, that code breaks the platforms and prevents them from moving on their own.

--- Levels/platform.gd
+++ Levels/platform.gd
@@ -4,7 +4,8 @@ extends KinematicBody2D
 export var startEnabled = false
 
 func _ready():
-	if startEnabled: $AnimationPlayer.play("platform")
-
-func _on_KeyPlatform_ActivatedMovingPlatforms():
+	#if startEnabled: 
 	$AnimationPlayer.play("platform")
+
+#func _on_KeyPlatform_ActivatedMovingPlatforms():
+	#$AnimationPlayer.play("platform")
