15b7728f2e46b07269a689b6b672f5a87140e783
Soup <bhcobb@mtu.edu>
2023-03-15 10:45:13 -0400

    add indev assets for puzzle system
    +key asset (plus import)
    +moving platforms assets
    +switch puzzle switch asset
    +switch puzzle door asset
    note most of these are unfinished and need shading + some stylistic tweaks, but they're better than the godot logo

--- Interactables/SwitchPuzzle.gd
+++ Interactables/SwitchPuzzle.gd
@@ -1,31 +1,19 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var SwitchOn = false
 var inRange = false
 
 signal switchActivated(check)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	$Sprite.modulate = Color.red
 
 func _input(event):
 	if(event.is_action_pressed("interact") && inRange):
+		$Sprite.modulate = Color.green if !SwitchOn else Color.red
 		SwitchOn = !SwitchOn
 		emit_signal("switchActivated",SwitchOn)
-
-
 
 func _on_Area2D_area_entered(area):
 	if(area.is_in_group("player")):
 		inRange = true
-	
--- Levels/platform.gd
+++ Levels/platform.gd
@@ -1,20 +1,10 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
-	$AnimationPlayer.play("platform")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-
-
+	$AnimationPlayer.stop(false)
 
 func _on_KeyPlatform_ActivatedMovingPlatforms():
 	$AnimationPlayer.play("platform")
-	pass
