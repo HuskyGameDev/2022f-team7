944ee0a5a4099dec5d3f94f88a2f71b99d7ad4b0
Andrew Tran <altran@mtu.edu>
2023-03-23 16:22:17 -0400

    pegasus debugging and extra stuff
    pegasus is buggier than i realized and will have to finish debugging next sprint, still waiting on the sprites for pegasus to finalize it but its not reacting to anything rn but i might have a solution for later

--- Enemies/pegasusEnemy.gd
+++ Enemies/pegasusEnemy.gd
@@ -11,55 +11,53 @@ var attacking = false
 var attackDirection
 var hit
 var location
-var returning
+var returning = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-func _physics_process(delta):
-	match(mode):
-		0:
-			var playerPos = $"../player".get_global_position() - self.global_position
-			if engaged:
-				if playerPos.length < 1:
-					pass
-				else:
-					vec = (position - player.position).normalized()
-					vec *= -0.2
-					move_and_collide(vec, false)
-		1:#MUST be a child of a pathfollower!
-			self.get_parent().unit_offset += (.0005 * railSpeed)
-		2:
-			pass #have not finished yet, no enemies use it either
-		3:
-			customMode(delta)
+var returnVec = Vector2.ZERO
+
 
 func _onStartEnter(body):
-	._onStartEnter(body)
-	if(!aimed && body.is_in_group("player") && charge):
+	if(body.get_class() == "KinematicBody2D"):
+		player = body
+	if( !(aiming || aimed) && charge):
+		engaged = false
 		charge = false
 		location = self.get_global_position()
 		$AimTimer.start()
 		aiming = true
-		
+		print("aiming peg")
+	elif(!charge && returning):
+		engaged = true
+
 	
 	
-func custom(delta):
+func customMode(delta):
+	if(engaged && player != null):
+		_onStartEnter(player)
+	
 	if(aiming): rotatePegasus()
-	if(attacking): hit = move_and_collide(attackDirection * speed)
-	if(returning): position = position.move_toward(location, speed*delta)
+	
+	if(attacking): 
+		returnVec = move_and_slide(attackDirection * speed)
+		if(returnVec.length() < 50): #if going too slow
+			pass
+
+	if(returning):
+		returnVec = move_and_slide(attackDirection * speed)
+		if(returnVec.length() < 50): #if going too slow
+			pass
 	.customMode(delta)
 	
 	
 func rotatePegasus():
 	direction = Vector2($"../player".get_global_position().x, $"../player".get_global_position().y) # Get player location
-	angle = get_global_position().angle_to_point(direction) + (1.0/2.0)*PI # Get angle to point toward player
+	angle = get_global_position().angle_to_point(direction) + (3.0/2.0)*PI # Get angle to point toward player
 	# Adjust rotation/angle so that it doesn't rotate the long way around
 	if abs(rotation - angle) >= PI && rotation != 0:
 		if rotation > angle:
-			rotation = (fposmod(rotation + PI, 2.0*PI) - PI) - (2.0 * PI)
+			rotation = (fposmod(rotation + PI, 2.0*PI) - PI) 
 		else:
-			angle = (fposmod(angle + PI, 2.0*PI) - PI) - (2.0 * PI)
+			angle = (fposmod(angle + PI, 2.0*PI) - PI) 
 	# Set rotation
 	if(rotation > angle + aimSpeed): rotation -= aimSpeed
 	if(rotation < angle - aimSpeed): rotation += aimSpeed
@@ -70,6 +68,7 @@ func _on_hitbox_area_entered(area):
 		returnPosition()
 	
 func returnPosition():
+	attackDirection = get_global_position().direction_to(location).normalized()
 	returning = true
 	$chargeCooldown.start()
 	
@@ -77,12 +76,13 @@ func _on_chargeCooldown_timeout():
 	charge = true
 
 func _on_aimTime_timeout():
+	print("aiming peg stop")
 	aiming = false
 	aimed = true
 	$chargeUp.start()
 	
 func _on_chargeUp_timeout():
-	attacking = true
 	attackDirection = get_global_position().direction_to(direction).normalized()
+	attacking = true
 	$animatedSprite.frame = engaged
 	
