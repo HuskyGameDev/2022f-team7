32dc973575a4a23d973c676197a89268eb2e90d7
Soup of the tomato kind <bhcobb@mtu.edu>
2023-09-21 15:38:22 -0400

    even more polish!
    +replaced old health bar with a newer one
    +fixes various ninepatches experiencing shrinkage, causing buttons to go off the background

--- Player/health.gd
+++ Player/health.gd
@@ -1,8 +1,4 @@
 extends Control
 
-#amount of pixels per heart (subject to change based on actual pixel sizes used) (x direction btw)
-var heartSize = 16
-
-#if there is some sort of change to the players health whether it be a heal or damaged
 func _on_player_health_changed(player_hearts):
-	$hearts.rect_size.x = player_hearts*heartSize
+	$hearts.frame = player_hearts
